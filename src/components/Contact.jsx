ae2ea9d500634cfcb37f2af4d3bcb6c8f2991626
ny <ny.kim664@gmail.com>
2025-07-10 16:39:32 +0900

    연락처 완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,66 @@
 import React from "react";
 
+
+const contactText = [
+    {
+        link:"http://"
+        title:"kk"
+    },
+    {
+        link:"mailto:111@naver.com",
+        title:"mail:111@naver.com",
+    }
+];
+
+
+
 const Contact = () => {
-    return <div>Contact</div>
+    return (
+         <section id="contact">
+            <div className="contact__inner">
+                <h2 className="contact__title">Contact</h2>
+                <div className="contact__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+
+                <div className="contact__text">
+                    <div className="text">
+                        {contactText.map((contact, key) => (
+                            <div key={key}>
+                                <a href={contact.link}
+                                rel="noopener noreferrer"
+                                target="_blank">
+                                    {contact.title}
+                                </a>
+                            </div>
+                        ))}
+
+
+                        <div>
+                            <a href="https://www.aaa" target="_blank" rel="noreferrer">kk</a> 
+                        </div>
+                        <div>
+                            <a href="mailto:111@naver.com" target="_blank" rel="noreferrer">mail : 111@NAVER.COM</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="contact__lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+         </section>
+    )
 };
 
 export default Contact;
