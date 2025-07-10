70fc3f8cbf979a610b624ef6ec5c62e514258e12
ny <ny.kim664@gmail.com>
2025-07-10 14:19:28 +0900

    헤더완료

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomeView from "./views/HomeView";
+// import HomeView from "./views/HomeView";
 
 import SignUp from './components/SignUp';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomeView />} />
+          {/* <Route path="/" element={<HomeView />} /> */}
         </Routes>
       </BrowserRouter>
       <SignUp />
+
+
+      <Header />
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
+
+
+const headerNav = [
+    {
+        title:"intro",
+        url:"#intro",
+    },
+    {
+        title:"skill",
+        url:"#skill",
+    },
+    {
+        title:"site",
+        url:"#site",
+    },
+    {
+        title:"portfolio",
+        url:"#port",
+    },
+    {
+        title:"contact",
+        url:"#contact",
+    },
+];
 
 const Header = () => {
+    const [show, setShow] = useState(false);
+
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    }
+
     return (
-        <div>Header</div>
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <a href="/">portfolio</a>
+                </div>
+
+                <nav className={`header__nav ${show ? "show":""}`} role="navigation" aria-label="메인메뉴">
+                    {/* <ul>
+                        <li><a href="#intro">intro</a></li>
+                        <li><a href="#skill">skill</a></li>
+                        <li><a href="#site">site</a></li>
+                        <li><a href="#port">portfolio</a></li>
+                        <li><a href="#contact">contact</a></li>
+                    </ul> */}
+
+                    <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    
+                </nav>
+                <div className="header__nav__mobile"
+                id="headerToggle"
+                aria-controls="primary-menu"
+                aria-expanded={show ? "true":"false"}
+                role="button"
+                tabindex="0"
+                onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
     )
 };
 
