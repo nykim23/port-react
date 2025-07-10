ba310951ba7e42d2d4738d1f7900b8cdb28b282e
ny <ny.kim664@gmail.com>
2025-07-10 16:48:31 +0900

    푸터 완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,94 @@
 import React from "react";
 
+
+
+const footerText = [
+    {
+        title:"youtube",
+        desc:"유투브",
+        link: "https://www.youtube.com"
+    },
+    {
+        title:"github",
+        desc:"유투브",
+        link: "https://www.youtube.com"
+    },
+    {
+        title:"gsap",
+        desc:"유투브",
+        link: "https://www.youtube.com"
+    },
+    {
+        title:"react",
+        desc:"유투브",
+        link: "https://www.youtube.com"
+    },
+    {
+        title:"vue",
+        desc:"유투브",
+        link: "https://www.youtube.com"
+    },
+    {
+        title:"next",
+        desc:"유투브",
+        link: "https://www.youtube.com"
+    },
+]
+
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer id="footer" role="contentinfo">
+            <div className="footer__inner">
+                <div className="footer__text">
+                    <span>ny</span>
+                    <span>kim</span>
+                </div>
+                <div className="footer__info">
+                    <div className="left">
+                        <div className="title">
+                            <a href="/">sign up</a>
+                        </div>
+                        <p>회원가입을 하시면....</p>
+                    </div>
+
+                    <div className="right">
+                        <h3>social</h3>
+
+                        
+
+                        <ul>
+
+                            {footerText.map((footer,key) => (
+                                <li key={key}>
+                                    <a href={footer.link}>{footer.title}</a>
+                                    <em>{footer.desc}</em>
+                                </li>
+                            ))}
+                            {/* <li>
+                                <a href="http://www.youtube.com">youtube</a>
+                                <em>안녕하세요</em>
+                            </li>
+                            <li>
+                                <a href="http://www.youtube.com">youtube</a>
+                                <em>안녕하세요</em>
+                            </li>
+                            <li>
+                                <a href="http://www.youtube.com">youtube</a>
+                                <em>안녕하세요</em>
+                            </li>
+                            <li>
+                                <a href="http://www.youtube.com">youtube</a>
+                                <em>안녕하세요</em>
+                            </li> */}
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer__right">
+                    리액트
+                </div>
+            </div>
+        </footer>
     );
 };
 
